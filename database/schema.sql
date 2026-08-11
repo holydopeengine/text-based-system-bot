9b1249b2118727fb6cdcc6cdeef53193add91388
holydopeengine <109796839+holydopeengine@users.noreply.github.com>
2026-08-11 12:21:52 +0000

    Set up projectand postgreSQL

---
+++ database/schema.sql
@@ -0,0 +1,8 @@
+CREATE TABLE menu_items (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    description TEXT,
+    price NUMERIC(10, 2) NOT NULL,
+    available BOOLEAN NOT NULL DEFAULT TRUE,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
